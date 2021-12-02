c17b42f563ade273e5e8f61b055fc4eff8f014d2
Jake Patch <jake@Jakes-Mac-mini.local>
2021-12-02 21:37:35 +0000

    Complete Day 1

--- days/day_one.php
+++ days/day_one.php
@@ -1,16 +1,22 @@
 <?php
 
-$file = file_get_contents("puzzle.txt");
+$file = file_get_contents("puzzles/day_1_puzzle.txt");
 $depths = explode("\n", $file);
-depth_increase($depths);
 
-function depth_increase($depths) {
-    $differences = array_filter($depths, function (int $value, $key) use ($depths): int {
-        return !($key == 0) && $value > $depths[$key - 1];
-    }, ARRAY_FILTER_USE_BOTH);
-    echo count($differences);
+echo "Initial Depth Increase: " . count(depth_increase($depths)) . "<br>";
+echo "Triple Measurements Count: " . count(depth_increase(tri_measurements($depths)));
+
+function tri_measurements($depths): array {
+    return array_map(function ($values, $key) use ($depths) {
+        return array_sum(array_slice($depths, $key, 3));
+    }, $depths, array_keys($depths));
 }
 
+function depth_increase($depths) {
+    return array_filter($depths, function (int $value, $key) use ($depths): int {
+        return !($key == 0) && $value > $depths[$key - 1];
+    }, ARRAY_FILTER_USE_BOTH);
+}
 
 
 
--- days/day_two.php
+++ days/day_two.php
@@ -1,19 +1,2 @@
 <?php
 
-$file = file_get_contents("puzzle.txt");
-$depths = explode("\n", $file);
-
-depth_increase(tri_measurements($depths));
-
-function tri_measurements($depths): array {
-    return array_map(function ($values, $key) use ($depths) {
-        return array_sum(array_slice($depths, $key, 3));
-    }, $depths, array_keys($depths));
-}
-
-function depth_increase($depths) {
-    $differences = array_filter($depths, function (int $value, $key) use ($depths): int {
-        return !($key == 0) && $value > $depths[$key - 1];
-    }, ARRAY_FILTER_USE_BOTH);
-    echo count($differences);
-}
