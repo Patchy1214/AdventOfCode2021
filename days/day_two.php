306576e91979d9c3d7c3f539685ae78fbc2471c7
Jake Patch <jake@Jakes-Mac-mini.local>
2021-12-02 21:31:43 +0000

    Day 2

--- days/day_two.php
+++ days/day_two.php
@@ -2,7 +2,14 @@
 
 $file = file_get_contents("puzzle.txt");
 $depths = explode("\n", $file);
-depth_increase($depths);
+
+depth_increase(tri_measurements($depths));
+
+function tri_measurements($depths): array {
+    return array_map(function ($values, $key) use ($depths) {
+        return array_sum(array_slice($depths, $key, 3));
+    }, $depths, array_keys($depths));
+}
 
 function depth_increase($depths) {
     $differences = array_filter($depths, function (int $value, $key) use ($depths): int {
