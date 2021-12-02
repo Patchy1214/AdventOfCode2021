a4c3fed75217ea0d77c907333b54a28be68e5ef2
Jake Patch <jake@Jakes-Mac-mini.local>
2021-12-02 21:53:26 +0000

    Day2 Part 1

--- days/day_two.php
+++ days/day_two.php
@@ -1,2 +1,21 @@
 <?php
 
+$file = file_get_contents("puzzles/day_2_puzzle.txt");
+$directions = explode("\n", $file);
+
+$position = get_positions($directions);
+
+function get_positions($directions) {
+    $position = [0, 0]; //[0] - Horizontal Position, [1] - Depth
+    foreach ($directions as $direction) {
+        $data = explode(" ", $direction);
+        if ($data[0] == "forward") {
+            $position[0] += (int) $data[1];
+        } else if ($data[0] == "down") {
+            $position[1] += (int) $data[1];
+        } else if ($data[0] == "up") {
+            $position[1] -= (int) $data[1];
+        }
+    }
+    return $position[0] * $position[1];
+}
